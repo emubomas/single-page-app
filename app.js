42454c39a29447a206241dd8465f5498009e5977
emubomas <33139194+emubomas@users.noreply.github.com>
2021-04-20 10:56:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+// import router from "./router";
+
+// (async function () {
+//   // 👉 Replace this with client creation 👈
+//    // handle user navigation
+//    window.addEventListener("hashchange", router);
+//    window.addEventListener("load", router);
+
+//    //handle user reload of browser
+//    if (sessionStorage.getItem("reload")) await router();
+//    sessionStorage.setItem("reload", "true");
+//  })();
+
 import router from "./router";
 
 (async function () {
-  // 👉 Replace this with client creation 👈
-   // handle user navigation
-   window.addEventListener("hashchange", router);
-   window.addEventListener("load", router);
-
-   //handle user reload of browser
-   if (sessionStorage.getItem("reload")) await router();
-   sessionStorage.setItem("reload", "true");
+    const domain = window.env.AUTH0_DOMAIN;
+    const client_id = window.env.CLIENT_ID;
+    const redirect_uri = window.env.APP_URL;
+    
+    window.auth0Client = await createAuth0Client({
+        domain,
+        client_id,
+        redirect_uri,
+        });
  })();
-
-
-  
   
